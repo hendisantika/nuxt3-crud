75d1b777ec05b9e5fff6be55fd1943ac1c47da99
hendisantika <hendisantika@yahoo.co.id>
2023-10-22 06:47:51 +0700

    #12 - Modify post/edit ✏️📗:octocat:⬆

--- pages/posts/edit/[id].vue
+++ pages/posts/edit/[id].vue
@@ -1,9 +1,103 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+
+//meta title
+useHead({
+  title: 'Edit a Post - hendisantika',
+});
+
+//init config
+const config = useRuntimeConfig();
+
+//init router
+const router = useRouter();
+
+//init route
+const route = useRoute();
+
+//fetch daa for get detail data post
+const {data: post}: any = await useFetch(`${config.public.apiBase}/api/posts/${route.params.id}`);
+
+
+//define state
+const image = ref('');
+const title = ref(post.value.data.title);
+const content = ref(post.value.data.content);
+const errors: any = ref({});
+
+//method for handle file changes
+const handleFileChange = (e: any) => {
+
+  //assign file to state
+  image.value = e.target.files[0]
+}
+
+//method "updatePost"
+const updatePost = async () => {
+
+  //init formData
+  let formData = new FormData();
+
+  //assign state value to formData
+  formData.append('image', image.value);
+  formData.append('title', title.value);
+  formData.append('content', content.value);
+  formData.append('_method', 'PATCH');
+
+  //store data with API
+  await $fetch(`${config.public.apiBase}/api/posts/${route.params.id}`, {
+
+    //method
+    method: 'POST',
+
+    //data
+    body: formData
+  })
+      .then(() => {
+        //redirect
+        router.push({path: "/posts"});
+      })
+      .catch((error) => {
+
+        //assign response error data to state "errors"
+        errors.value = error.data
+      });
+}
+
+</script>
 
 <template>
-  <div>
-    Page: foo
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card border-0 rounded shadow">
+          <div class="card-body">
+            <form @submit.prevent="updatePost()">
+              <div class="mb-3">
+                <label class="form-label fw-bold">Image</label>
+                <input class="form-control" type="file" @change="handleFileChange($event)">
+                <div v-if="errors.image" class="alert alert-danger mt-2">
+                  <span>{{ errors.image[0] }}</span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label class="form-label fw-bold">Title</label>
+                <input v-model="title" class="form-control" placeholder="Title Post" type="text">
+                <div v-if="errors.title" class="alert alert-danger mt-2">
+                  <span>{{ errors.title[0] }}</span>
+                </div>
+              </div>
+              <div class="mb-3">
+                <label class="form-label fw-bold">Content</label>
+                <textarea v-model="content" class="form-control" placeholder="Content Post" rows="5"></textarea>
+                <div v-if="errors.content" class="alert alert-danger mt-2">
+                  <span>{{ errors.content[0] }}</span>
+                </div>
+              </div>
+              <button class="btn btn-md btn-primary rounded-sm shadow border-0" type="submit">Update</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped></style>
