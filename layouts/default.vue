1a83b9102211e64bf8303ce75b92b799794ee0f7
hendisantika <hendisantika@yahoo.co.id>
2023-10-22 06:26:10 +0700

    #4 - Add navbar ✏️📗:octocat:⬆

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,35 @@
 
 <template>
   <div>
-    Layout: default
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+      <div class="container">
+        <NuxtLink class="navbar-brand" to="/">HOME</NuxtLink>
+        <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse"
+                type="button">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarSupportedContent" class="collapse navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <NuxtLink aria-current="page" class="nav-link active" to="/posts">POSTS</NuxtLink>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0" role="search">
+            <a class="btn btn-success" href="https://santrikoding.com" target="_blank">SANTRIKODING.COM</a>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!--- render page -->
     <slot/>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  background-color: lightgray;
+  font-family: 'Quicksand', sans-serif
+}
+</style>
