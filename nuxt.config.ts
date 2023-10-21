3b829931e093037e5169c56d05183ac655512e60
hendisantika <hendisantika@yahoo.co.id>
2023-10-22 06:30:21 +0700

    #7 - Set base URL API ✏️📗:octocat:⬆

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
+    runtimeConfig: {
+        public: {
+            apiBase: 'http://localhost:8000'
+        }
+    },
     app: {
         head: {
             link: [
