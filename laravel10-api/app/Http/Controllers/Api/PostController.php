c8d70d2011fbb6ff328391a4519201cb023597f5
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:30:07 +0700

    #27 - Add update api ✏️📗:octocat:⬆

--- laravel10-api/app/Http/Controllers/Api/PostController.php
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -73,4 +73,57 @@ class PostController extends Controller
         //return single post as a resource
         return new PostResource(true, 'Detail Data Post!', $post);
     }
+
+    /**
+     * update
+     *
+     * @param mixed $request
+     * @param mixed $post
+     * @return void
+     */
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            //delete old image
+            Storage::delete('public/posts/' . basename($post->image));
+
+            //update post with new image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+
+        } else {
+
+            //update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content,
+            ]);
+        }
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
 }
