8d77e8469aaa060f70a1b91ce22f011f219c18a0
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:18:21 +0700

    #19 - Modify Post Migration ✏️📗:octocat:⬆

--- laravel10-api/database/migrations/2023_10_24_111704_create_posts_table.php
+++ laravel10-api/database/migrations/2023_10_24_111704_create_posts_table.php
@@ -12,6 +12,9 @@ return new class extends Migration {
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('image');
+            $table->string('title');
+            $table->text('content');
             $table->timestamps();
         });
     }
