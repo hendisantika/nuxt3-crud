ec8a079c8e1bde14a8848468933f7e2a35e92c71
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:33:52 +0700

    #28 - Add destroy api ✏️📗:octocat:⬆

--- laravel10-api/app/Http/Controllers/Api/PostController.php
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
@@ -125,5 +126,27 @@ class PostController extends Controller
 
         //return response
         return new PostResource(true, 'Data Post Berhasil Diubah!', $post);
+    }
+
+    /**
+     * destroy
+     *
+     * @param mixed $post
+     * @return void
+     */
+    public function destroy($id)
+    {
+
+        //find post by ID
+        $post = Post::find($id);
+
+        //delete image
+        Storage::delete('public/posts/' . basename($post->image));
+
+        //delete post
+        $post->delete();
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Dihapus!', null);
     }
 }
