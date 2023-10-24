fd6c25441be77233a955df54d43895c6df3b6a55
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:19:04 +0700

    #20 - Modify Post Model ✏️📗:octocat:⬆

--- laravel10-api/app/Models/Post.php
+++ laravel10-api/app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'image',
+        'title',
+        'content',
+    ];
 }
