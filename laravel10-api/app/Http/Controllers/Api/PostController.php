cc25a849ba64e69ada9b67355f4dbfe354fedcb5
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:24:52 +0700

    #23 - Modify PostController.php ✏️📗:octocat:⬆

--- laravel10-api/app/Http/Controllers/Api/PostController.php
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //get all posts
+        $posts = Post::latest()->paginate(5);
+
+        //return collection of posts as a resource
+        return new PostResource(true, 'List Data Posts', $posts);
+    }
 }
