4d6786fd4b1bacf5353ff8683cc7229ad06cc8e8
hendisantika <hendisantika@yahoo.co.id>
2023-10-24 18:28:08 +0700

    #25 - Add store api ✏️📗:octocat:⬆

--- laravel10-api/app/Http/Controllers/Api/PostController.php
+++ laravel10-api/app/Http/Controllers/Api/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -20,5 +22,40 @@ class PostController extends Controller
 
         //return collection of posts as a resource
         return new PostResource(true, 'List Data Posts', $posts);
+    }
+
+    /**
+     * store
+     *
+     * @param mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        //return response
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan!', $post);
     }
 }
